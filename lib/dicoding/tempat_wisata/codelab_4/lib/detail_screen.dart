3afd8c3c84d96e7512ec78420001a5b7e2812f84
ryanroshandy55 <ryanroshandy7@gmail.com>
2024-12-30 00:28:18 +0700

    New Updates Codelab_4

--- lib/dicoding/tempat_wisata/codelab_4/lib/detail_screen.dart
+++ lib/dicoding/tempat_wisata/codelab_4/lib/detail_screen.dart
@@ -10,6 +10,25 @@ class DetailScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 800) {
+          return DetailWebPage(place: place);
+        } else {
+          return DetailMobilePage(place: place);
+        }
+      }
+    );
+  }
+}
+
+class DetailMobilePage extends StatelessWidget{
+  final TourismPlace place;
+
+  const DetailMobilePage({Key? key, required this.place}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: SingleChildScrollView(
@@ -154,6 +173,141 @@ class _FavoriteButtonState extends State<FavoriteButton>{
             isFavorite = !isFavorite;
           });
         },
+      )
+    );
+  }
+}
+
+class DetailWebPage extends StatelessWidget {
+  final TourismPlace place;
+
+  const DetailWebPage({Key? key, required this.place}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Wisata Bandung',
+            style: TextStyle(
+              fontFamily: 'Staatliches',
+              fontSize: 32,
+            ),
+          ),
+          const SizedBox(height: 32),
+          Row(
+            children: [
+              const Text(
+                'Wisata Bandung',
+                style: TextStyle(
+                  fontFamily: 'Staatliches',
+                  fontSize: 32,
+                ),
+              ),
+              const SizedBox(height: 32,),
+              Row(children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(place.imageAsset),
+                      ),
+                      const SizedBox(height: 16,),
+                      Container(
+                        height: 150,
+                        padding: const EdgeInsets.only(bottom: 16),
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: place.imageUrls.map((url) { 
+                            return Padding(
+                              padding: const EdgeInsets.all(4.0),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(url),
+                              ),
+                            );
+                          }).toList(),
+                      ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 32,),
+                Expanded(
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Text(
+                            place.name,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 30.0,
+                              fontFamily: 'Staatliches',
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: <Widget> [
+                                  const Icon(Icons.calendar_today),
+                                  const SizedBox(width: 8.0,),
+                                  Text(
+                                    place.openDays, 
+                                    style: informationTextStyle,
+                                  ),
+                                ],
+                              ),
+                              const FavoriteButton(),
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              const Icon(Icons.access_time),
+                              const SizedBox(width: 8.0),
+                              Text(
+                                place.openTime,
+                                style: informationTextStyle,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 8.0,),
+                          Row(
+                            children: <Widget> [
+                              const Icon(Icons.monetization_on),
+                              const SizedBox(width: 8.0),
+                              Text(
+                                place.ticketPrice,
+                                style: informationTextStyle,
+                              )
+                            ],
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(vertical: 16.0),
+                            child: Text(
+                              place.description,
+                              textAlign: TextAlign.justify,
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                fontFamily: 'Oxygen',
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],)
+            ],
+          ),
+        ],
       )
     );
   }
